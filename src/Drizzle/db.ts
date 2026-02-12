889c20ff82505051fd813cecea27e26e8e32b7ac
flaviasudo <kihahuflavia@gmail.com>
2026-02-12 13:14:16 +0300

    DATABASE_URL to Database_URL in db.ts

--- src/Drizzle/db.ts
+++ src/Drizzle/db.ts
@@ -3,6 +3,6 @@ import { drizzle } from "drizzle-orm/neon-http"
 import * as schema from "./schema"
 import { neon } from "@neondatabase/serverless"
 
-export const client = neon(process.env.DATABASE_URL as string)
+export const client = neon(process.env.Database_URL as string)
 const db = drizzle(client, { schema, logger: true });
 export default db;
